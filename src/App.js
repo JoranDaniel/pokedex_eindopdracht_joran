da578834c6895b8b6be9f058ce24efdadc4a9e31
Joran Daniël <124999321+JoranDaniel@users.noreply.github.com>
2023-04-20 18:05:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+  };
+export default App;
